a13480809c9b2088b6783b62c297f32784191bb0
Artem Krol <artemkrol47@gmail.com>
2020-06-30 09:35:54 +0300

    Clock

---
+++ Network/Clock/ClockForm.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace Clock
+{
+    partial class ClockForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelTime = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.buttonGet = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // labelTime
+            // 
+            this.labelTime.AutoSize = true;
+            this.labelTime.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelTime.Location = new System.Drawing.Point(21, 20);
+            this.labelTime.Name = "labelTime";
+            this.labelTime.Size = new System.Drawing.Size(51, 16);
+            this.labelTime.TabIndex = 0;
+            this.labelTime.Text = "Time :";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(116, 20);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(127, 22);
+            this.textBox1.TabIndex = 1;
+            // 
+            // buttonGet
+            // 
+            this.buttonGet.Location = new System.Drawing.Point(168, 87);
+            this.buttonGet.Name = "buttonGet";
+            this.buttonGet.Size = new System.Drawing.Size(75, 23);
+            this.buttonGet.TabIndex = 2;
+            this.buttonGet.Text = "Get";
+            this.buttonGet.UseVisualStyleBackColor = true;
+            this.buttonGet.Click += new System.EventHandler(this.buttonGet_Click);
+            // 
+            // ClockForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(255, 122);
+            this.Controls.Add(this.buttonGet);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.labelTime);
+            this.Name = "ClockForm";
+            this.Text = "Clock";
+            this.Load += new System.EventHandler(this.ClockForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelTime;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button buttonGet;
+    }
+}
+
---
+++ Network/Clock/ClockForm.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Clock
+{
+    public partial class ClockForm : Form
+    {
+        // Локальный порт для прослушивания
+        private static int localPort;
+        // Удаленный IP и порт для подключения
+        private static IPAddress remoteIPAddress;
+        private static int remotePort;
+
+        public ClockForm()
+        {
+            InitializeComponent();
+        }
+
+        private void ClockForm_Load(object sender, EventArgs e)
+        {
+
+            localPort = Convert.ToInt32("5001");
+            remoteIPAddress = IPAddress.Parse("127.0.0.1");
+            remotePort = Convert.ToInt32("5001");
+
+            var threadReceiver = new Thread(new ThreadStart(Receiver));
+            threadReceiver.IsBackground = true;
+            threadReceiver.Start();
+        }
+        private static void Send()
+        {
+            DateTime date = DateTime.Now;
+            // Создаем UdpClient
+            var sender = new UdpClient();
+
+            // Создаем endPoint по информации об удаленном хосте
+            var endPoint = new IPEndPoint(remoteIPAddress, remotePort);
+
+            try
+            {
+                // Преобразуем данные в массив байтов
+                byte[] bytes = Encoding.UTF8.GetBytes(date.ToString("T"));
+
+                // Отправляем данные
+                sender.Send(bytes, bytes.Length, endPoint);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ошибка сокета: " + ex.Message);
+            }
+            finally
+            {
+                // Закрываем соединение
+                sender.Close();
+            }
+        }
+        public void Receiver()
+        {
+            // Создаем UdpClient для чтения входящих данных
+            var receivingUdpClient = new UdpClient(localPort);
+
+            IPEndPoint RemoteIpEndPoint = null;
+
+            try
+            {
+                while (true)
+                {
+                    // Ожидание дейтаграммы
+                    byte[] receiveBytes = receivingUdpClient.Receive(ref RemoteIpEndPoint);
+
+                    // Преобразуем и отображаем данные
+                    string returnData = Encoding.UTF8.GetString(receiveBytes);
+
+                    textBox1.Invoke((MethodInvoker)(() => textBox1.Text = returnData));
+                    //if (returnData != null)
+                     //   break;
+                }
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show("Ошибка сокета: " + ex.Message);
+            }
+        }
+
+        private void buttonGet_Click(object sender, EventArgs e)
+        {
+            Send();
+        }
+    }
+}
