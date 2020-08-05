a00159a15846f2e636e866d3e95be31e1b4e0c67
Artem Krol <artemkrol47@gmail.com>
2020-08-05 10:55:58 +0300

    example 4 Commond + HW3 Commond+Banding

--- Wpf/HW/HW3/MainWindow.xaml.cs
+++ Wpf/HW/HW3/MainWindow.xaml.cs
@@ -15,6 +15,7 @@ using System.Windows.Shapes;
 
 namespace HW3
 {
+
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
@@ -23,11 +24,54 @@ namespace HW3
         public MainWindow()
         {
             InitializeComponent();
+
+            //var bilding = new CommandBinding(ApplicationCommands.Open);
+            //bilding.Executed += one_Click;
+            //CommandBindings.Add(bilding);
         }
 
+        /* private void one_Click(object sender, RoutedEventArgs e)
+         {
+             RadioButton ee = new RadioButton();
+             try
+             {
+                 ee = e.Source as RadioButton;
+                 //MessageBox.Show(ee.Name, e.Source.ToString());
+                 if (ee.Name == "radio1")
+                     tab1.IsSelected = true;
+             }
+             catch(Exception ex)
+             {
+                 MessageBox.Show(ex.Message, "exption");
+             }
+         }*/
         private void one_Click(object sender, RoutedEventArgs e)
         {
-            tabOne.IsSelected = true;
+            tab1.IsSelected = true;
+        }
+        private void two_Click(object sender, RoutedEventArgs e)
+        {
+            tab2.IsSelected = true;
+        }
+        private void tree_Click(object sender, RoutedEventArgs e)
+        {
+            tab3.IsSelected = true;
+        }
+        private void four_Click(object sender, RoutedEventArgs e)
+        {
+            tab4.IsSelected = true;
+        }
+        private void five_Click(object sender, RoutedEventArgs e)
+        {
+            tab5.IsSelected = true;
+        }
+        private void six_Click(object sender, RoutedEventArgs e)
+        {
+            tab6.IsSelected = true;
+        }
+        private void seven_Click(object sender, RoutedEventArgs e)
+        {
+            tab7.IsSelected = true;
         }
     }
 }
