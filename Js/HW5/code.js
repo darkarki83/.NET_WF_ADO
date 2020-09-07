43feeedbfd2e7b9f475d1041fd9226c4a1fec8bd
Artem Krol <artemkrol47@gmail.com>
2020-09-07 09:37:05 +0300

    HW5 + HW6 JS

--- Js/HW5/code.js
+++ Js/HW5/code.js
@@ -1,11 +1,38 @@
 "use strict"
 
-function HW5(idd) 
+function HW5(src) 
 {
-    window.open('index2.html');
-    console.log(`${idd}`);
-   // document.write(`<img style="width: 30px; height: 30px; src="image/3.jpg" alt="1"> "<br>`);
+    let win = window.open();
+    win.document.write(`<h2>Выбери меня</h3>`);
+    win.document.write(`<img style="width: 500px; height: 500px;" src=${src}  alt="1">  <br>`);   
 
-    //document.one2.src = "image/2.jpg";
-    console.log(`${idd}`);
-}
+    //let win = window.open('index2');
+    //win.document.write(`<img style="width: 500px; height: 500px;" src=${src}  alt="1">  <br>`);   
+    
+};
+
+let tooltipElem;
+    
+document.onmouseover = function(event) {
+    let target = event.target;
+    console.log(target);
+    // если у нас есть подсказка...
+    let tooltipHtml = target.dataset.tooltip;
+    //console.log(`1 + ${tooltipHtml}`);
+    if (!tooltipHtml) return;
+    
+    //console.log(mytext);
+    //console.log(tooltipHtml);
+    mytext.innerText = tooltipHtml;    
+};
+    
+document.onmouseout = function(e) {
+    if(mytext.target != '') {
+        mytext.innerText = '';
+    }
+};
+/*function Back(src) 
+{
+   //window.close('index2.html');
+}*/
+
---
+++ Js/HW6/code.js
@@ -0,0 +1,50 @@
+"use strict"
+
+    let currentImage = 0, count = 0;
+    let path = ["image/1.jpg", "image/2.png", "image/3.jpg", "image/4.jpg", "image/5.jpg", "image/6.png", "image/7.jpg", "image/8.jpg", "image/9.jpg", "image/10.png"];
+    let img = [];
+
+    for (var i = 0; i < path.length; i++) {
+        img[i] = new Image (500, 500);
+        img[i].src = path[i];
+        img[i].onload = countImages;
+    }
+
+    function countImages() {
+        count++;
+        if (count == path.length) {
+            info.innerText = "Готово!";
+        } else {
+            info.innerText = `Загрузка изображений: ${count} из ${path.length}`;
+        }
+    }
+
+    function nextImage() {
+        currentImage++;
+        if (currentImage == path.length) {
+            currentImage = 0;
+        }
+        document.images["picture"].src = img[currentImage].src;
+        console.log(picture);
+        return false;
+    }
+
+    document.onclick = function(event) 
+    {
+        let target = event.target;
+
+        //console.log(target.alt);
+        if(target.src != undefined){
+            currentImage = parseInt(target.alt) - 1;
+            document.images["picture"].src = img[currentImage].src;
+        }
+        return false;
+    };
+
+    /*function HW5(alt) 
+    {
+        currentImage = alt - 1;
+        document.images["picture"].src = img[currentImage].src;
+        return false;
+    };*/
+
