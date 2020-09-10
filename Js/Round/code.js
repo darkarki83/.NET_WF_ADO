fe340576ca464420d1df9a7ff6f0c7e7ecdb7baf
Artem Krol <artemkrol47@gmail.com>
2020-09-10 10:05:39 +0300

    clock+

---
+++ Js/Round/code.js
@@ -0,0 +1,48 @@
+"use strict"
+
+const delay = 30;                    // Задержка (в 
+const R = 120;                        // Радиус 
+const da = 3 * Math.PI / 180;        // Приращение 
+let a = 0;     // Угол (в 
+
+let Arr = [];                      
+function moveElem(a = 0, b = 0) {
+    if(Arr[0] == Number){
+    elemToMove.style.left = Arr[0] + R * Math.cos(a) + "px";
+    elemToMove.style.top = Arr[1] + R * Math.sin(a) + "px";
+    }
+    else{
+        elemToMove.style.left = a + R * Math.cos(a) + "px";
+        elemToMove.style.top = b + R * Math.sin(a) + "px";
+    }
+
+    a += da;
+}
+/*function mouseMove(e) {
+    //elemToMove.left = MouseEvent.
+
+    Arr[0] = e.pageX; // Координата X курсора
+    Arr[1] = e.pageY; // Координата X курсора
+  console.log(`X :  ${e.pageX}", Y : ${e.pageY}`)
+}*/
+//page.onmousemove = mouseMove;
+
+document.onmousemove = function(e) {
+        Arr[0] = e.pageX; // Координата X курсора
+        Arr[1] = e.pageY; // Координата Y курсора
+      console.log(`X :  ${e.pageX}", Y : ${e.pageY}`)
+};
+
+/*function mouseCoords(e) {
+    // Для браузера IE
+    if (document.all)  { 
+        elemToMove.left = event.x + document.body.scrollLeft; 
+      y = event.y + document.body.scrollTop; 
+    // Для остальных браузеров
+    } else {
+      x = e.pageX; // Координата X курсора
+      y = e.pageY; // Координата Y курсора
+    }
+    document.getElementById("coords").innerHTML = "X : " + x + ", Y : " + y;
+    console.log(`X :  ${x}", Y : ${y}`)
+   }*/
