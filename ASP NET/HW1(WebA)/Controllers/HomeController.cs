a7f852b90f9fc1d5518b90665d0de170fba087d2
Artem Krol <artemkrol47@gmail.com>
2020-10-23 09:39:37 +0300

    hw1 Asp

---
+++ ASP NET/HW1(WebA)/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using HW1_WebA_.Models;
+
+namespace HW1_WebA_.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Answer(string num1, string num2, string action)
+        {
+            ViewData["num1"] = num1;
+            ViewData["num2"] = num2;
+            ViewData["action"] = action;
+            return View();
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ ASP NET/HW1(WebA)/Helpers/CalcHelper.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HW1_WebA_.Helpers
+{
+    public static class CalcHelper
+    {
+        public static HtmlString Calculate(this IHtmlHelper html, int num1, int num2, string action)
+        {
+
+            double sum = 0;
+            if(action == "+")
+            {
+                sum = num1 + num2;
+            }
+            else if (action == "-")
+            {
+                sum = num1 - num2;
+            }
+            else if (action == "*")
+            {
+                sum = num1 * num2;
+            }
+            else if (action == "/")
+            {
+                sum = (double)num1 / (double)num2;
+            }
+            return new HtmlString(sum.ToString());
+        }
+
+    }
+}
