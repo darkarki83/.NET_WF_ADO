cb82abc487ade5d5ea0c8ffcd3c4b6d6b0ae99ee
Artem Krol <60381661+darkarki83@users.noreply.github.com>
2020-12-12 02:41:37 +0300

    HW8 Asp

---
+++ ASP NET/HW8(DB)/HW8(DB)/Models/Product.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HW8_DB_.Models
+{
+    public class Product
+    {
+        [Required(ErrorMessage = "Required field")]
+        [Remote("CheckProduct", "Home", ErrorMessage = "there is a product with that name")]
+        [DataType(DataType.Text)]
+        [Display(Name = "Name *")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Required field")]
+        [DataType(DataType.Currency)]
+        [Display(Name = "Cost *")]
+        public decimal Cost { get; set; }
+
+        [Required(ErrorMessage = "Required field")]
+        [StringLength(400, MinimumLength = 5,  ErrorMessage = "Description length from 5 to 400 characters")]
+        [DataType(DataType.MultilineText)]
+        [Display(Name = "Description *")]
+        public string Description { get; set; }
+
+
+        [DataType(DataType.Date)]
+        [Display(Name = "Add Date")]
+        public DateTime Date { get; set; }
+
+    }
+}
